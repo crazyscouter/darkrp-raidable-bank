850123ae98bb633f94bd66c699b802acfb0ad452
crazyscouter <crazyscouter1@gmail.com>
2016-11-07 18:03:08 -0500

    Update

--- darkrp_raidablebank/lua/autorun/sh_bank.lua
+++ darkrp_raidablebank/lua/autorun/sh_bank.lua
@@ -18,6 +18,6 @@ bankConfig.bankGuardWeapons = {"weapon_ar2"};	// What weapons do bank security g
 bankConfig.initialMoney = 1000;						// How much money does the bank start out with.
 bankConfig.moneyGrowth = 200;						// How much money does the bank get every interval of time.
 bankConfig.moneyGrowthTime = 4;					// How long does it take for the bank to increase its worth. --Should be in seconds!
-bankConfig.robTime = 10;
+bankConfig.robTime = 120;
 bankConfig.maxMoney = 20000;					//How high should the vault's storage go
 bankConfig.minMoney = 2000;						//Min money required for raiding
