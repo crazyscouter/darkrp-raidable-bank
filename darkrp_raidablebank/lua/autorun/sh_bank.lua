ccb303f8be868b4722f7c9d6516398335bd5ba21
John Salisbury <crazyscouter1@gmail.com>
2017-01-30 19:31:38 -0500

    Formatting update

--- darkrp_raidablebank/lua/autorun/sh_bank.lua
+++ darkrp_raidablebank/lua/autorun/sh_bank.lua
@@ -1,23 +1,25 @@
-bankConfig = {};
+bankConfig = {}
 
 bankConfig.bank = {
 	Vector( -3024.888428, -3327.741211, 0.263596), 
 	Vector(-4235.753906, -3577.024902, 332.977539)
-};												// Where is the bank located.
+}										// Where is the bank located.
 bankConfig.vault = {
 	Vector(-4235.753906, -3577.024902, 332.977539), 
 	Vector(-3717.154541, -4346.132324, 18.334160)
 }
 
+//Where should the banner which shows the vault amount be located?
 bankConfig.bannerPos = Vector(-2816, -2930, 250.183456)
 bankConfig.bannerAngle = Angle(0, 135, 90)
 
+//What is considered a bank guard? 
 bankConfig.bankGuards = {TEAM_POLICE, TEAM_MAYOR}
 
-bankConfig.bankGuardWeapons = {"weapon_ar2"};	// What weapons do bank security get.
-bankConfig.initialMoney = 1000;						// How much money does the bank start out with.
-bankConfig.moneyGrowth = 200;						// How much money does the bank get every interval of time.
-bankConfig.moneyGrowthTime = 4;					// How long does it take for the bank to increase its worth. --Should be in seconds!
-bankConfig.robTime = 120;
-bankConfig.maxMoney = 20000;					//How high should the vault's storage go
-bankConfig.minMoney = 2000;						//Min money required for raiding
+bankConfig.bankGuardWeapons = {"weapon_ar2"}	// What weapons do bank security get.
+bankConfig.initialMoney = 1000						// How much money does the bank start out with.
+bankConfig.moneyGrowth = 200						// How much money does the bank get every interval of time.
+bankConfig.moneyGrowthTime = 4					// How long does it take for the bank to increase its worth. --Should be in seconds!
+bankConfig.robTime = 120
+bankConfig.maxMoney = 20000					//How high should the vault's storage go
+bankConfig.minMoney = 2000						//Min money required for raiding
